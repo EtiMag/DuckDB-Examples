7e6d26a8d73b338b8449703f169fb36b110af035
EtiMag <etienne.maeght@ensae.fr>
2023-08-06 21:11:16 +0000

    create schema before importing views

--- PostgreSQL/read_from_PostgreSQL.py
+++ PostgreSQL/read_from_PostgreSQL.py
@@ -13,15 +13,18 @@ with open(f"{os.path.dirname(__file__)}/connection.yml", "r") as connection_file
     port: int = connection_parameters["port"]
     source_schema: str = connection_parameters["source_schema"]
 
-sink_schema: str = "dbschema"   # schema name to use to create views in DuckDB
 
-duckdb.execute(  # register postgreSQL tables as views in DuckDB
+# register postgreSQL tables as views in DuckDB
+# from schema source_schema, into schema sink_schema
+sink_schema: str = "new_schema"
+duckdb.execute(f"CREATE SCHEMA IF NOT EXISTS {sink_schema}")
+
+duckdb.execute(
     f"CALL POSTGRES_ATTACH('dbname={database_name} host={host} port={port} "
     f"user={user} password={password}', "
-    f"source_schema={source_schema}, sink_schema={sink_schema});"
-)
+    f"source_schema={source_schema}, sink_schema={sink_schema}, overwrite=True)"
+)  # overwrite = True means that existing views in schema sink_schema should be overwritten
 
 # Show all tables and views in DuckDB
-duckdb.sql("SELECT * FROM duckdb_tables").show()
-duckdb.sql("PRAGMA show_tables").show()
 
+duckdb.sql("SELECT * FROM information_schema.tables").show()
