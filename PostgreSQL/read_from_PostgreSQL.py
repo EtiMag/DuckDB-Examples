ea4a2d948f22b8f13f74b872b639a9b772bf9894
EtiMag <etienne.maeght@ensae.fr>
2023-08-06 20:30:07 +0000

    debug & refactor

--- PostgreSQL/read_from_PostgreSQL.py
+++ PostgreSQL/read_from_PostgreSQL.py
@@ -1,22 +1,27 @@
+import os
+import yaml
 import duckdb
 
 duckdb.execute("LOAD postgres")
 
-host: str = "hostname"
-database_name: str = "postgresql-db"
-user: str = "user-example"
-password: str = "password"
-port: int = 5432
+with open(f"{os.path.dirname(__file__)}/connection.yml", "r") as connection_file:
+    connection_parameters = yaml.safe_load(connection_file)
+    host: str = connection_parameters["host"]
+    database_name: str = connection_parameters["database_name"]
+    user: str = connection_parameters["user"]
+    password: str = connection_parameters["password"]
+    port: int = connection_parameters["port"]
+    source_schema: str = connection_parameters["source_schema"]
 
-source_schema: str = "public"  # schema of the postgres database to load from
-sink_schema: str = "public"   # schema name to use to create views in DuckDB
+sink_schema: str = "dbschema"   # schema name to use to create views in DuckDB
 
 duckdb.execute(  # register postgreSQL tables as views in DuckDB
-    f"CALL POSTGRES_ATTACH('dbname={database_name}, host={host}, port={port}, "
-    f"user={user}, password={password}', "
+    f"CALL POSTGRES_ATTACH('dbname={database_name} host={host} port={port} "
+    f"user={user} password={password}', "
     f"source_schema={source_schema}, sink_schema={sink_schema});"
 )
 
 # Show all tables and views in DuckDB
-duckdb.execute("SELECT * FROM duckdb_tables;").show()
-duckdb.execute("PRAGMA show_tables;").show()
+duckdb.sql("SELECT * FROM duckdb_tables").show()
+duckdb.sql("PRAGMA show_tables").show()
+
